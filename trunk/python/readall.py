edb756ccd711c593d7238dc5cce1fcad86814c08
jestabro@ncsa.illinois.edu <jestabro@ncsa.illinois.edu@a3e1c6df-86ea-e2b0-193d-5698befe6888>
2011-05-15 02:05:15 +0000

    Restore readall.py example.

--- trunk/python/readall.py
+++ trunk/python/readall.py
@@ -1,4 +1,4 @@
-"""test.py: """
+"""readall.py: Read all tcp_estats variables from all connections."""
 
 from Web10G import *
 
@@ -7,17 +7,15 @@ a = Web10Gagent()
 cl = a.all_connections()
 
 for c in cl:
-    print("here, what")
-    print("cid %i"%c.cid)
 
     print("Connection %d (%s %d  %s %d)"%(c.cid, \
                                         c.read('LocalAddress'), \
                                         c.read('LocalPort'), \
                                         c.read('RemAddress'), \
                                         c.read('RemPort')))
-    print("and here's a uint64: %d"%(c.read('ThruOctetsReceived')))
-#    for (name, val) in c.readall().items():
-#        print("%-20s %s"%(name, str(val)))
-#    print('')
+
+    for (name, val) in c.readall().items():
+        print("%-20s %s"%(name, str(val)))
+    print('')
 
 
