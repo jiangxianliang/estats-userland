53c89f602de7a1877a5972e207bfe933509b3387
jestabro@ncsa.illinois.edu <jestabro@ncsa.illinois.edu@a3e1c6df-86ea-e2b0-193d-5698befe6888>
2011-05-16 12:47:44 +0000

    Remove deprecated func.

--- trunk/lib/estats/var-int.h
+++ trunk/lib/estats/var-int.h
@@ -26,7 +26,6 @@
 # define CHECK_VAR(x)
 #endif
 
-//estats_error* _estats_var_size_from_type(int* size, ESTATS_TYPE type);
 void          _estats_var_dep_check(estats_var* var);
 estats_error* _estats_var_next_undeprecated(estats_var** next, const estats_var* prev);
 
